55d7edb6337c8a3955fe83a5f0edff04c79fbb84
Neil <133841195+neilvaltec@users.noreply.github.com>
2025-09-14 15:36:28 -0400

    chore: modify the first linear part to parabola for Cardinal Primitive

--- Test/bird_lib/dds.c
+++ Test/bird_lib/dds.c
@@ -31,11 +31,12 @@ void dds_init(void) {
         chirp_table[x] = float2fix15( 0.03015*x*x + 2000 );
     }
     
-    // Cardinal Primitive 1: First Linear (downward sweep) - Key 3
+    // Cardinal Primitive 1: Inverted Parabola (left half only) - Key 3
     for (int x = 0; x < freq_table_size; x++) {
         float normalized_x = (float)x / freq_table_size;
-        // Linear downward: 8kHz to 2.8kHz (higher frequency range)
-        float freq = 8000 - 5200 * normalized_x;
+        // Inverted parabola taking only left half: 8kHz to 2.8kHz
+        // Left half of U-curve: starts gradual, then accelerates
+        float freq = 8000 - 5200 * (2 * normalized_x - normalized_x * normalized_x);
         cardinal_linear_1_table[x] = float2fix15(freq);
     }
     
